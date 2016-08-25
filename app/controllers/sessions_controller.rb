5cf2b9b9ff5469d569ce40d8f7a6a9ec77eea722
jbreiger <jbreiger@uw.edu>
2016-08-25 12:43:45 -0700

    added navbar and some other small changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
 		user = User.find_by_username(params[:username])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
+			session[:user_name]= user.username
 			redirect_to '/bets/create'
 		else
 			flash[:error] = 'Login info wrong!'
