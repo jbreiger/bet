fe139139a560010920c2668be06abfcc82c4aa4f
jbreiger <jbreiger@uw.edu>
2016-08-25 09:28:22 -0700

    small changes before pull

--- app/controllers/user_bets_controller.rb
+++ app/controllers/user_bets_controller.rb
@@ -5,7 +5,7 @@ def cashout
 	user= user_bet.user
 	wager= user_bet.amount
 	pick= user_bet.winner
-
+	
 	if user_bet.check == 1
 		puts "In the user bet"
 		flash[:message] = ["You already got your money for this game"]	
@@ -62,62 +62,60 @@ def create
 	#check(@bet)
 	@user= User.find(session[:user_id])
 	@user_money= @user.money
-	
+	display_bet= params[:amount].to_f
+	amount_bet= -(params[:amount].to_f)
+	new_total= amount_bet.to_f + @user.money
+
 	 if @user && @bet
 		 UserBet.create(user: @user, bet: @bet, amount: params[:amount], winner: params[:winner])
 	end
-	
-	#@userbet= Userbet.find(user: @user, bet: @bet)
-	#@userbet.bet
 
-	# puts @bet.team1_goal, "team1goal"
-	# puts @bet.team2_goal, "team2goal"
-	# puts @bet.home_line, "homeline"
-	# puts params[:amount], "amount bet"
-	if @bet.team1_goal == -1
+	 if @bet.team1_goal == -1
 		puts @bet.team1_goal 
-		flash[:message] = ["Game hasnt happened yet your bet is saved"]
+		@user.update(money: new_total)
+		flash[:message] = "Your $#{display_bet}0 bet is saved! Your balance is now $#{new_total}0 Check your user page after match has happened to cash out!"
 		redirect_to "/bets/#{@bet.id}"
-	else
-		puts @bet.team1_goal
-		
-		flash[:message] << "Game has happened"
-
-	if @bet.team1_goal.to_i > @bet.team2_goal.to_i
-		outcome= "home"
-		puts "home won"
-	elsif @bet.team1_goal.to_i < @bet.team2_goal.to_i
-		outcome= "away"
-		puts "away won"
-	else
-		outcome= "draw"	
-		puts "draw"			
-	end
-	
-	@bet.update(winner: outcome)
-
-	if outcome== "home" && params[:winner]== "home"
-		@wager= (params[:amount]).to_f * (@bet.home_line).to_f
-		puts "home team won and selected home team"
-		flash[:message]= ["You Won!"]
-	elsif outcome== "away" && params[:winner]=="away"
-		@wager= (params[:amount]).to_f * (@bet.away_line).to_f
-		flash[:message]= ["You Won!"]
-	elsif outcome== "draw" && params[:winner]== "draw"
-		@wager= (params[:amount]).to_f * (@bet.draw_line).to_f	
-		flash[:message] << "You Won!"	
-	else	
-		@wager= -(params[:amount].to_f)	
-		flash[:message] << "You Lost!"
 	end	
-	if @wager
-		@user_money += @wager
-		@user.update(money: @user_money)
+	# else
+	# 	puts @bet.team1_goal
+		
+	# 	flash[:message] << "Game has happened"
 
-	end
-		redirect_to "/bets/#{@bet.id}"
+	# if @bet.team1_goal.to_i > @bet.team2_goal.to_i
+	# 	outcome= "home"
+	# 	puts "home won"
+	# elsif @bet.team1_goal.to_i < @bet.team2_goal.to_i
+	# 	outcome= "away"
+	# 	puts "away won"
+	# else
+	# 	outcome= "draw"	
+	# 	puts "draw"			
+	# end
+	
+	# @bet.update(winner: outcome)
 
-	end
+	# if outcome== "home" && params[:winner]== "home"
+	# 	@wager= (params[:amount]).to_f * (@bet.home_line).to_f
+	# 	puts "home team won and selected home team"
+	# 	flash[:message]= ["You Won!"]
+	# elsif outcome== "away" && params[:winner]=="away"
+	# 	@wager= (params[:amount]).to_f * (@bet.away_line).to_f
+	# 	flash[:message]= ["You Won!"]
+	# elsif outcome== "draw" && params[:winner]== "draw"
+	# 	@wager= (params[:amount]).to_f * (@bet.draw_line).to_f	
+	# 	flash[:message] << "You Won!"	
+	# else	
+	# 	@wager= -(params[:amount].to_f)	
+	# 	flash[:message] << "You Lost!"
+	# end	
+	# if @wager
+	# 	@user_money += @wager
+	# 	@user.update(money: @user_money)
+
+	# end
+	# 	redirect_to "/bets/#{@bet.id}"
+
+	# end
 	#@game= @game.first
 	
 	
